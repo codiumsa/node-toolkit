b0dbb049cedcb7b608036686bf1aa2a84bf9ce82
Jorge Ramírez <jorgeramirez1990@gmail.com>
2017-10-27 18:57:46 -0300

    docs(auth): Se agrega documentación al middleware de autenticación
    refs #4804

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,3 +1,17 @@
+/**
+ * Este middleware puede utilizarse para agregar control de autenticación vía JWT
+ * a los endpoint de la api.
+ * 
+ * @example
+ * 
+ * const authenticated = require('node-toolkit/middleware/authenticated');
+ * const Koa = require('koa');
+ * const app = new Koa();
+ * 
+ * app.use(authenticated.unless({ path: [/^\/p/] }));
+ * 
+ * @since 0.1.0
+ */
 const jwt = require('koa-jwt');
 const path = require('path');
 var appDir = path.dirname(require.main.filename);
